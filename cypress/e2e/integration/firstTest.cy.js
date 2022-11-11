c37a838d288226f0baf35173de4cf21050c1f1bb
apuryasova <a.puryasova@mish.design>
2022-11-11 12:17:20 +0100

    add to lecture 2 cod

--- cypress/e2e/integration/firstTest.cy.js
+++ cypress/e2e/integration/firstTest.cy.js
@@ -1,45 +1,45 @@
-/// <reference types = "Cypress"/>
+/ <reference types = "Cypress"/>
 
-// describe('My First Tests', () => {
-//     it('By ID', () => {
-//       cy.visit('https://www.facebook.com/')
-//       cy.get('#email')
-//     })
-//   })
+describe('My First Tests', () => {
+    it('By ID', () => {
+      cy.visit('https://www.facebook.com/')
+      cy.get('#email')
+    })
+  })
 
-//   it('By Class', () => {
-//     cy.visit('https://docs.cypress.io/api/commands/get#Syntax')
-//     cy.get('.DocSearch-Button-Placeholder')
-//   })
+  it('By Class', () => {
+    cy.visit('https://docs.cypress.io/api/commands/get#Syntax')
+    cy.get('.DocSearch-Button-Placeholder')
+  })
 
-//   it('By Tag', () => {
-//     cy.visit('https://docs.cypress.io/api/commands/get#Syntax')
-//     cy.get('nav')
-//   })
+  it('By Tag', () => {
+    cy.visit('https://docs.cypress.io/api/commands/get#Syntax')
+    cy.get('nav')
+  })
 
-//   it('By Tag value', () => {
-//     cy.visit('https://www.facebook.com/')
-//     cy.get('[name="pass"]')
-//   })
+  it('By Tag value', () => {
+    cy.visit('https://www.facebook.com/')
+    cy.get('[name="pass"]')
+  })
   
-//   it('By Diffrent Tag', () => {
-//     cy.visit('https://www.facebook.com/')
-//     cy.get('[role="button"][href="#"]')
-//   })
+  it('By Diffrent Tag', () => {
+    cy.visit('https://www.facebook.com/')
+    cy.get('[role="button"][href="#"]')
+  })
 
-//   it('By Diffrent Types', () => {
-//     cy.visit('https://docs.cypress.io/api/commands/get#Syntax')
-//     cy.get('button[type="button"][aria-label="Search"]')
-//   })
+  it('By Diffrent Types', () => {
+    cy.visit('https://docs.cypress.io/api/commands/get#Syntax')
+    cy.get('button[type="button"][aria-label="Search"]')
+  })
 
-//   it.only('By Contains name', () => {
-//     cy.visit('https://next.privat24.ua/')
-//     cy.get('*[class^="card"]')
-//   })
+  it.only('By Contains name', () => {
+    cy.visit('https://next.privat24.ua/')
+    cy.get('*[class^="card"]')
+  })
   
-//   ;
+  ;
 
-//Comment 2
+
 
   it('Using Get with Find and Eq', () => {
     cy.visit('https://next.privat24.ua/deposit/open/')
