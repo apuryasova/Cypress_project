bb609c6faf7b0955d14956d50d8ae7d0c945655f
apuryasova <a.puryasova@mish.design>
2022-11-18 15:20:40 +0100

    Add elements

--- cypress/e2e/integration/firstTest.cy.js
+++ cypress/e2e/integration/firstTest.cy.js
@@ -1,4 +1,10 @@
 /// <reference types = "Cypress"/>
 
 
- 
+ // type
+
+ it(('tupe', ()=>{
+  cy.visit('https://next.privat24.ua/mobile?lang=en')
+  .get('[data-qa-node="phone-number"]')
+  .type(112233344)
+ }))
